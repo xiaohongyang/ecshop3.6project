5f2cb138057ad347055c48166ac2421b0d4e7301
xiaohongyang <xiaohongyang@adsage.com>
2017-04-11 16:25:42 +0800

    ecshop installed

---
+++ ecshop_3.6/ecshop/h5/config/config.app.js
@@ -0,0 +1,11 @@
+// https://api.ecshop36.geek-zoo.net
+// https://ecshoptest.xyunqi.com
+var GLOBAL_CONFIG = {
+	'API_HOST': 'http://api.ecshop.local',
+	'APP_NAME': 'Demo',
+	'APP_DESC': 'This is a demo',
+	'APP_KEYWORDS': 'Demo, Shop',
+	'DEBUG': false,
+	'ENCRYPTED': false,
+	'FOR_WEIXIN': false
+};
---
+++ ecshop_3.6/ecshop/h5/config/config.app.sample.js
@@ -0,0 +1,10 @@
+var GLOBAL_CONFIG = {
+	'API_HOST': 'https://api.ecshop36.geek-zoo.net',
+	'APP_NAME': 'Demo',
+	'APP_DESC': 'This is a demo',
+	'APP_KEYWORDS': 'Demo, Shop',
+	'DEBUG': false,
+	'ENCRYPTED':false,
+	 //如果公众号没有配置，将for_weixin置为false，才能在微信中使用
+	'FOR_WEIXIN':false 
+};
