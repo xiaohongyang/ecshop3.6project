48414e13a9b671da83d5d54e393ec42fe7492b01
xiaohongyang <xiaohongyang@adsage.com>
2017-05-13 21:49:26 +0800

    获取指定商品的关联商品信息

--- ecshop_3.6/ecshop/includes/lib_insert.php
+++ ecshop_3.6/ecshop/includes/lib_insert.php
@@ -140,6 +140,46 @@ function insert_menu_cur_class($arr)
         return '';
 }
 
+function insert_link_goods($arr) {
+    $id = 0;
+    if(!key_exists('id', $arr)){
+        return '';
+    } else {
+        $id = intval($arr['id']);
+    }
+    $sql = "SELECT
+                distinct goods.goods_id, goods.goods_thumb
+            FROM
+                `ecs_link_goods` AS link 
+            LEFT JOIN ecs_goods as goods
+            on link.link_goods_id = goods.goods_id
+            where is_delete = 0 and (link.goods_id={$id} or goods.goods_id={$id})";
+
+    $res = $GLOBALS['db']->GetAll($sql);
+    $str = <<<STD
+        <div id="colorArea">
+            {itemStr}
+        </div>
+        <div class="divclear"></div>
+STD;
+
+
+
+    if(is_array($res) && count($res)){
+
+        $itemStr = "";
+        foreach ($res as $item){
+
+            $selectcolordiv = $item['goods_id'] == $id ? "Selectcolordiv" :'';
+            $clickEvent = $item['goods_id'] == $id ? "" :'onclick="window.location.href=\'goods.php?id='.$item['goods_id'].'\'"';
+            $itemStr.= ("<div id=\"color_{$item['goods_id']}\" class=\"colordiv {$selectcolordiv} \" {$clickEvent}>
+                            <img src=\"{$item['goods_thumb']}\" alt=\"\">
+                        </div>");
+        }
+    }
+    $str = str_replace('{itemStr}', $itemStr, $str);
+    return $str;
+}
 
 
 /**
